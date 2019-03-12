1d455efc878d819e50d4d38cc510a5970bf51390
Daniel Meléndez <30836176+DannyMelendez@users.noreply.github.com>
2019-03-12 08:52:56 -0400

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,33 @@
+<head>
+    <title>Make My Sandwich</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
+    <style type="text/css">
+    .brand-text{
+        color: #AE825D !important;
+    }
+    form{
+        max-width: 460px;
+        margin: 20px auto;
+        padding: 20px;
+    }
+    .sandwich {
+        width: 130px;
+        margin: 40px auto -90px;
+        display: block;
+        position: relative;
+        top: -45px;
+    }
+    </style>   
+</head>
+    <body class="grey lighten-4">
+        <nav class="white z-depth-0">
+            <div class="container">
+                <a href="index.php" class="brand-logo brand-text">Make My Sandwich</a>
+                <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li><a href="add.php" class="btn brand z-depth-0">Add a Sandwich</a></li>
+                </ul>
+            </div>
+        </nav>
